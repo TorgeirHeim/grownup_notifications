97cb9f5a4ee59ea5959a3c7834c576e1443b0d88
Torgeir Heim Pedersen <torgeirpc@gmail.com>
2024-12-17 09:53:45 +0100

    Fix oldDestroy is undefined

--- extension.js
+++ extension.js
@@ -1,29 +1,30 @@
 import * as MessageTray from 'resource:///org/gnome/shell/ui/messageTray.js';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
-// import MessageTray from 'ui://messageTray';
-
-// const MessageTray = imports.ui.messageTray;
-// const MessageTray = MessageTray.MessageTray;
 
 let oldDestroy;
-  
+
 function _destroy(reason) {
-  if (reason != 3) {
-    oldDestroy.call(this);
-  }
+    if (reason != MessageTray.NotificationDestroyedReason.SOURCE_CLOSED) {
+        console.log("Destroying notification for reason: " + reason);
+        oldDestroy.call(this);
+    }
 }
 
 export default class GrownNotifications extends Extension {
-  
-  init() {
-    oldDestroy = MessageTray.Notification.prototype.destroy;
-  }
-  
-  enable() {
-    MessageTray.Notification.prototype.destroy = _destroy;
-  }
-  
-  disable() {
-    MessageTray.Notification.prototype.destroy = oldDestroy;
-  }
+    init() {
+        oldDestroy = MessageTray.Notification.prototype.destroy;
+    }
+
+    enable() {
+        if (!oldDestroy) {
+            this.init(); // Ensure oldDestroy is initialized
+        }
+        MessageTray.Notification.prototype.destroy = _destroy;
+    }
+
+    disable() {
+        if (oldDestroy) {
+            MessageTray.Notification.prototype.destroy = oldDestroy;
+        }
+    }
 }
